1216b40a9394c9d21b65ac3e4ebc14c85cd1522f
emontecalvo <emilymontecalvo@gmail.com>
2016-07-24 20:33:08 -0700

    initial sql notes

---
+++ sql_notes.rb
@@ -0,0 +1,17 @@
+# SQL NOTES: (taken from the book Learning SQL)
+
+# SELECT - the select clause determines which of all possible columns 
+# should be included in the query’s result set
+
+# VIEW - a view is a query that is stored in the data dictionary. 
+# It looks and acts like a table, but there is no data associated 
+# with a view (think of it as a virtual table).
+
+# WHERE - the where clause is the mechanism for filtering out unwanted 
+# rows from your result set, it contain a single filter condition
+
+
+
+# ORDER - the order by clause is the mechanism for sorting your result 
+# set using either raw column data or expressions based on column data
+
